687d63e790c0b55746223f6fc439bce65e638247
rayray39 <raynerlimfy@gmail.com>
2025-05-03 15:52:19 +0800

    Habits.tsx: Add buttons to toggle between habits

--- src/Components/Habits.tsx
+++ src/Components/Habits.tsx
@@ -1,14 +1,20 @@
 import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import Stack from "@mui/material/Stack"
 
 function Habits({ theme }:{ theme:boolean }) {
 
     return <Box sx={{
         border: theme ? '1px solid white' : '1px solid black',
         borderRadius: '8px',
-        width:'300px',
-        height:'300px'
+        width:'500px',
+        height:'500px'
     }}>
-
+        <Stack spacing={1}>
+            <Button disableElevation variant="contained">Habit 1</Button>
+            <Button disableElevation variant="contained">Habit 2</Button>
+            <Button disableElevation variant="contained">Habit 3</Button>
+        </Stack>
     </Box>
 }
 
