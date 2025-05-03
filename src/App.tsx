0057b1eb4c836d6cc4c5671a36b14cbe7cf783d6
rayray39 <raynerlimfy@gmail.com>
2025-05-03 15:00:35 +0800

    App.tsx: Add switch for toggling between light and dark modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
+import Box from "@mui/material/Box"
+import Switch from "@mui/material/Switch"
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { useState } from "react"
+import { createTheme, ThemeProvider, useColorScheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+function MyApp() {
+    // theme selected by the user from the switch component
+    const [theme, setTheme] = useState<boolean>(false);
+
+    // the color mode for material ui, based on selected theme
+    const { mode, setMode } = useColorScheme();
+    if (!mode) {
+        return null;
+    }
+
+    const handleNewTheme = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTheme(event.target.checked);
+
+        if (event.target.checked) {
+            setMode('dark');
+        } else {
+            setMode('light');
+        }
+    }
+
+    return <Box sx={{
+        display:'flex',
+        justifyContent:'center'
+    }}>
+        <FormControl>
+            <FormControlLabel 
+                control={<Switch value={theme} onChange={handleNewTheme} color="default"></Switch>} label='🌗'/>
+        </FormControl>
+    </Box>
+}
+
+const theme = createTheme({
+    colorSchemes: {
+      dark: true,
+    },
+});
 
 function App() {
-    return <>
-        hello world
-    </>
+    return <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <MyApp />
+    </ThemeProvider>
 }
 
 export default App
