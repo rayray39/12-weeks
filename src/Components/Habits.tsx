fc88d00e02fd9792061097eb4d9fde258ec6d9de
rayray39 <raynerlimfy@gmail.com>
2025-05-04 15:03:36 +0800

    NewHabitDialog.tsx: Remove button that adds new habit

--- src/Components/Habits.tsx
+++ src/Components/Habits.tsx
@@ -1,24 +1,13 @@
 import Box from "@mui/material/Box"
-import Button from "@mui/material/Button"
 import Stack from "@mui/material/Stack"
 
-function Habits({ theme, addNewHabit }:{ theme:boolean, addNewHabit:() => void }) {
-
-    const handleNewHabit = () => {
-        addNewHabit();
-        console.log('handle new habit');
-    }
+function Habits({ theme }:{ theme:boolean }) {
 
     return <Box sx={{
         display:'flex',
         flexDirection:'column',
         alignItems:'center'
     }}>
-        <Button
-            disableElevation
-            variant="contained"
-            onClick={handleNewHabit}>Add New Habit</Button>
-
         <Stack spacing={1} sx={{
             border: theme ? '1px solid white' : '1px solid black',
             borderRadius: '8px',
