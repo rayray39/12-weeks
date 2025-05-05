6b1d93bc0270887918ece8d6dfd5193956b05d7a
rayray39 <raynerlimfy@gmail.com>
2025-05-05 21:41:00 +0800

    Habits.tsx: Update display of habit as a new card component

--- src/Components/Habits.tsx
+++ src/Components/Habits.tsx
@@ -2,11 +2,18 @@ import Box from "@mui/material/Box"
 import Stack from "@mui/material/Stack"
 import { Habit } from "../Types/types"
 import Button from "@mui/material/Button"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import Typography from "@mui/material/Typography"
 
 // main display area for all the habits
 function Habits({ theme, habits }:{ theme:boolean, habits:Habit[] }) {
     const [displayedHabit, setDisplayedHabit] = useState<Habit>();
+
+    useEffect(() => {
+        setDisplayedHabit(habits[0]);
+    }, [])
 
     const handleSelectHabit = (index:number) => {
         console.log(`habit ${index} is being displayed`);
@@ -30,7 +37,12 @@ function Habits({ theme, habits }:{ theme:boolean, habits:Habit[] }) {
                 // border:'1px solid black',
                 padding:'20px',
                 width:'600px'
-            }}>{displayedHabit ? displayedHabit.title : 'main area'}</Box>
+            }}>{
+                displayedHabit ?
+                    <HabitCard title={displayedHabit.title} desc={displayedHabit.desc} /> :
+                    'no habits'
+                }
+            </Box>
            
             <Stack spacing={1} sx={{
                 // border:'1px solid black',
@@ -50,6 +62,19 @@ function Habits({ theme, habits }:{ theme:boolean, habits:Habit[] }) {
             </Stack>
         </Box>
     </Box>
+}
+
+function HabitCard({ title, desc }:{ title:string, desc:string }) {
+    return <Card>
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                {title}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                {desc}
+            </Typography>
+      </CardContent>
+    </Card>
 }
 
 export default Habits
